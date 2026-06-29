bc87245ac4e59df02d64d202a9ab0a755a596500
avbravo <avbravo@avbravo-ThinkCentre-M70t-Gen-3>
2026-06-29 09:00:54 -0500

    Nuevas anotaciones

--- buildandrun.sh
+++ buildandrun.sh
@@ -1,3 +1,3 @@
 mvn clean verify install
 
-java -jar -XX:+UseCompactObjectHeaders target/JettraWebBackEnd-1.0-SNAPSHOT.jar  
+java -jar -XX:+UseCompactObjectHeaders target/JettraWebBackEnd-1.0.0-SNAPSHOT.jar  
